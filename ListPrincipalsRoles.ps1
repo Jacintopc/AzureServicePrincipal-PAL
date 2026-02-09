175400b1dd7d4495b14b684b4b1edf1d6906499e
Jacinto Perez Calavera <jacintopc@hotmail.com>
2026-02-09 11:15:52 +0100

    Create ListPrincipalsRoles.ps1 script
    Add script to list role assignments for specific principals

---
+++ ListPrincipalsRoles.ps1
@@ -0,0 +1,4 @@
+# Prerequisites:  Install jq from https://jqlang.org/
+# jq is like sed for JSON data - you can use it to slice and filter and map and transform structured data with the same ease that sed, awk, grep and friends let you play with text.
+
+az role assignment list --all | jq '[.[] | select((.roleDefinitionName == "Owner" or .roleDefinitionName == "Contributor") and (.scope | contains("/resourceGroups/") | not)) | {Principal: .principalName, Tipo: .principalType, Rol: .roleDefinitionName, Scope: .scope}]' | ConvertFrom-Json | Format-Table -AutoSize
