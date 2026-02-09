87101e5bb40aeb824abf953f833f45e3928d6118
Jacinto Perez Calavera <jacintopc@hotmail.com>
2026-02-09 11:06:56 +0100

    Add PowerShell script for Azure management partner
    This script connects to Azure using a service principal, manages the SoftwareONE partner ID, and retrieves management partner information.

---
+++ PALCodeforSP.ps1
@@ -0,0 +1,36 @@
+# Requisitos (si no están instalados)
+
+# Install-Module -Name Az -AllowClobber -Scope CurrentUser
+# Install-Module -Name Az.ManagementPartner
+
+# The main variables for this PS is TenantID and Service Principle name
+
+$tenantId = "<tu-tenant-id>"
+$appId    = "<client-id-del-service-principal-SWONE>"
+$secret   = "<client-secret-del-service-principal-SWONE>"
+
+$MPNId = 3124318 # SoftwareONE MPN ID
+
+# Convertir el secreto en SecureString
+$secureSecret = ConvertTo-SecureString $secret -AsPlainText -Force
+
+# Crear el objeto de credenciales
+$credentials = New-Object System.Management.Automation.PSCredential($appId, $secureSecret)
+
+Connect-AzAccount -ServicePrincipal -Credential $credentials -Tenant $tenantId
+
+# Link to the SWONE Partner ID. The partner ID is the Microsoft Partner Network (MPN) ID for SWONE.
+
+$pal = Get-AzManagementPartner
+if ($pal) {
+    Remove-AzManagementPartner -PartnerId $pal.PartnerId -PassThru -Confirm:$false
+}
+Remove-AzManagementPartner -PartnerId $(Get-AzManagementPartner).PartnerId -PassThru -Confirm:$false
+new-AzManagementPartner -PartnerId $MPNId
+
+# Get-AzManagementPartner: obtiene el MPN Id y demás información del Management Partner asociado al usuario
+# o entidad de servicio autenticada en ese momento.
+Get-AzManagementPartner
+
+# Logout of the current account
+Disconnect-AzAccount
